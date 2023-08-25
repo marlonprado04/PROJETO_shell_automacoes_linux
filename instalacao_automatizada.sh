e296e9e65e833cc47503401304b1371252fccb32
Marlon Prado <marlonprado04@gmail.com>
2023-08-25 19:46:07 -0300

    feat: add missioncenter

--- instalacao_automatizada.sh
+++ instalacao_automatizada.sh
@@ -73,6 +73,7 @@ flatpak install com.jetbrains.IntelliJ-IDEA-Community -y 2>> "$LOG_FILE" || hand
 flatpak install com.calibre_ebook.calibre -y 2>> "$LOG_FILE" || handle_error "Falha ao instalar com.calibre_ebook.calibre via Flatpak."
 flatpak install com.valvesoftware.Steam -y 2>> "$LOG_FILE" || handle_error "Falha ao instalar com.valvesoftware.Steam via Flatpak."
 flatpak install org.eclipse.Java -y 2>> "$LOG_FILE" || handle_error "Falha ao instalar org.eclipse.Java via Flatpak."
+flatpak install io.missioncenter.MissionCenter -y 2>> "$LOG_FILE" || handle_error "Falha ao instalar io.missioncenter.MissionCenter via Flatpak."
 
 # -----------------------------------------------------------
 
