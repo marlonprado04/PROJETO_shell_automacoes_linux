08e65fe2777a0da054695664b537fbdc893c6030
Marlon Prado <marlonprado04@gmail.com>
2023-10-25 10:35:19 -0300

    docs: add coments

--- verificacao_de_repositorios.sh
+++ verificacao_de_repositorios.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Define a pasta onde estão localizados seus repositórios
+# Define variável para armazenar a pasta atual, onde estão localizados os repositórioss
 repos_folder="."
 
-# Loop para percorrer os subdiretórios (repositórios)
+# Cria loop para percorrer cada subdiretório da pasta atual
+# Sintaxe: para cada repositório, faça:
 for repo in "$repos_folder"/*; do
+    # Verifica se o diretório atualmente percorrido possui um subdiretório ".git"
+    # Sintaxe: se subdiretório ".git" existir, então:
     if [ -d "$repo/.git" ]; then
+        # Se sim, cria nova variável para armazenar o nome da pasta atual
+        # Sintaxe: variável recebe o nome base do repositório / pasta atual
         repo_name=$(basename "$repo")
+        # Imprime mensagem com "Repositório: <nome_do_repositório>"
         echo "Repositório: $repo_name"
+        # Entra na pasta específica e em seguida usa "git Status"
         (cd "$repo" && git status)
         echo
         echo "------------------------------------" 
         echo
     fi
 done
-
